6ed9d6e0feb5d0b570d6f0a3c7f760248db0478a
kakiche6 <arezki.inventeam@gmail.com>
2021-12-28 14:33:52 +0100

    add alert provider

--- src/App.js
+++ src/App.js
@@ -7,29 +7,31 @@ import About from './pages/About';
 // import User from './pages/User'
 import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext';
-// import { AlertProvider } from './context/alert/AlertContext'
+import { AlertProvider } from './context/alert/AlertContext';
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-          <main className='container mx-auto px-3 pb-12'>
-            {/* <Alert /> */}
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              {/* <Route path='/user/:login' element={<User />} /> */}
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='*' element={<NotFound />} />
-            </Routes>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              {/* <Alert /> */}
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                {/* <Route path='/user/:login' element={<User />} /> */}
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
